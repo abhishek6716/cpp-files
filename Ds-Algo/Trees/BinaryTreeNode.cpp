5a881ae1f61623d493be3bcbd5363886ec8c0a9c
Abhishek singh <abhisheksingh6716@gmail.com>
2020-10-05 12:38:08 +0530

    added more files

--- Ds-Algo/Trees/BinaryTreeNode.cpp
+++ Ds-Algo/Trees/BinaryTreeNode.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<stack>
 using namespace std;
 
 template<typename T>
@@ -250,6 +251,225 @@ BinaryTreeNode<int>* getTreefromPostandInOrder(int *postorder, int postLength, i
     int inEnd=inLength-1;
     return helper(postorder, postStart, postEnd, inorder, inStart, inEnd);
 }
+
+int height(BinaryTreeNode<int> *root){                                                   // height of tree
+    if(root==NULL){                                                                     
+        return 0;
+    }
+    return 1+height(root->left)+height(root->right);
+}
+int diameter(BinaryTreeNode<int> *root){                                                 // diameter of tree
+    if(root==NULL){                                                                      // complexity O(n*heightOfTree)
+        return 0;
+    }
+    int option1=height(root->left)+height(root->right);
+    int option2=diameter(root->left);
+    int option3=diameter(root->right);
+    return max(option1, max(option2, option3));
+}
+
+pair<int, int>  heightDiameter(BinaryTreeNode<int> *root){                               // diameter and height of tree
+    if(root==NULL){                                                                      // better complexity of O(n)
+        pair<int, int> p;
+        p.first=0;
+        p.second=0;
+        return p;
+    }
+    pair<int, int> leftAns=heightDiameter(root->left);
+    pair<int, int> rightAns=heightDiameter(root->right);
+    int ld=leftAns.second;
+    int lh=leftAns.first;
+    int rd=rightAns.second;
+    int rh=rightAns.first;
+
+    int height=1+max(lh, rh);
+    int diameter=max(lh+rh, max(ld, rd));
+    pair<int, int> p;
+    p.first=height;
+    p.second=diameter;
+    return p;
+}
+
+pair<int, int> minMax(BinaryTreeNode<int> *root){                                        // min max data of tree
+    if(root==NULL){
+        pair<int, int> p;
+        p.first=INT_MAX;
+        p.second=INT_MIN;
+        return p;
+    }
+    pair<int, int> leftAns=minMax(root->left);
+    pair<int, int> rightAns=minMax(root->right);
+    int lmin=leftAns.first;
+    int lmax=leftAns.second;
+    int rmin=rightAns.first;
+    int rmax=rightAns.second;
+
+    pair<int, int> p;
+    p.first=min(min(lmin, rmin), root->data);
+    p.second=max(max(lmax, rmax), root->data);
+    return p;
+}
+
+int SumOfAllNodes(BinaryTreeNode<int> *root){
+    if(root==NULL){
+        return 0;
+    }
+    int subAns1=SumOfAllNodes(root->left);
+    int subAns2=SumOfAllNodes(root->right);
+    return subAns1+subAns2+root->data;
+}
+
+int height(BinaryTreeNode<int> *root){                                                   // height of tree
+    if(root==NULL){                                                                     
+        return 0;
+    }
+    return 1+height(root->left)+height(root->right);
+}
+bool isBalanced(BinaryTreeNode<int> *root){                                              // isBalanced
+    if(root==NULL){                                                                      // not good complexity
+        return true;
+    }
+    int h1=height(root->left);
+    int h2=height(root->right);
+    if(h1-h2>1 || h1-h2<-1){
+        return false;
+    }
+    bool subAns1=isBalanced(root->left);
+    bool subAns2=isBalanced(root->right);
+    if(subAns1 && subAns2){
+        return true;
+    }
+    return false;
+}
+
+class BalancedReturnType{
+    public:
+    int height;
+    bool balanced;
+};
+BalancedReturnType* isBalancedHelper(BinaryTreeNode<int> *root){
+    if(root==NULL){
+        BalancedReturnType *ans=new BalancedReturnType();
+        ans->height=0;
+        ans->balanced=true;
+        return ans;
+    }
+    BalancedReturnType *ans1=isBalancedHelper(root->left);
+    BalancedReturnType *ans2=isBalancedHelper(root->right);
+    bool flag;
+    if(!(ans1->balanced) || !(ans2->balanced) || abs(ans1->height-ans2->height) > 1){
+        flag=false;
+    }
+    else{
+        flag=true;
+    }
+    BalancedReturnType *output=new BalancedReturnType();
+    output->height=max(ans1->height, ans2->height)+1;
+    output->balanced=flag;
+    return output;
+}
+bool isBalanced(BinaryTreeNode<int> *root){                                              // better complexity
+    if(root==NULL){
+        return true;
+    }
+    return isBalancedHelper(root)->balanced;
+}
+
+void PrintLevelWise(BinaryTreeNode<int> *root){                                          // print level wise
+    queue<BinaryTreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+    pendingNodes.push(NULL);
+    while(pendingNodes.size()!=0){
+        BinaryTreeNode<int> *front=pendingNodes.front();
+        pendingNodes.pop();
+        if(front==NULL){
+            cout<<endl;
+            if(pendingNodes.size()!=0){
+                pendingNodes.push(NULL);
+            }
+        }
+        else{
+            cout<<front->data<<" ";
+            if(front->left!=NULL){
+                pendingNodes.push(front->left);
+            }
+            if(front->right!=NULL){
+                pendingNodes.push(front->right);
+            }
+        }
+    }
+}
+
+BinaryTreeNode<int>* removeLeafNodes(BinaryTreeNode<int> *root){                         // remove leaf nodes
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->left==NULL && root->right==NULL){
+        delete root;
+        return NULL;
+    }
+    root->left=removeLeafNodes(root->left);
+    root->right=removeLeafNodes(root->right);
+    return root;
+}
+
+void zigzagOrder(BinaryTreeNode<int> *root){                                             // print tree in zigzag order
+    if(root==NULL){
+        return;
+    }
+    stack<BinaryTreeNode<int>*> currentlevel;
+    stack<BinaryTreeNode<int>*> nextlevel;
+    currentlevel.push(root);
+    bool leftToright=true;
+    while(currentlevel.size()!=0){
+        BinaryTreeNode<int> *temp=currentlevel.top();
+        currentlevel.pop();
+        if(temp!=NULL){
+            cout<<temp->data<<" ";
+            if(leftToright==true){
+                if(temp->left!=NULL){
+                    nextlevel.push(temp->left);
+                }
+                if(temp->right!=NULL){
+                    nextlevel.push(temp->right);
+                }
+            }
+            else{
+                if(temp->right!=NULL){
+                    nextlevel.push(temp->right);
+                }
+                if(temp->left!=NULL){
+                    nextlevel.push(temp->left);
+                }
+            }
+        }
+        if(currentlevel.size()==0){
+            cout<<endl;
+            leftToright=false;
+            swap(currentlevel, nextlevel);
+        }
+    }
+}
+
+void NodesWithoutSibling(BinaryTreeNode<int> *root){                                     // print nodes withot siblings
+    if(root==NULL){
+        return;
+    }
+    if(root->left==NULL && root->right==NULL){
+        return;
+    }
+    if(root->left!=NULL && root->right==NULL){
+        cout<<root->left->data<<endl;
+    }
+    if(root->left==NULL && root->right!=NULL){
+        cout<<root->right->data<<endl;
+    }
+
+    NodesWithoutSibling(root->left);
+    NodesWithoutSibling(root->right);
+}
+
+
 /* 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1 */ 
 int main(){
     /*
@@ -262,10 +482,15 @@ int main(){
     BinaryTreeNode<int> *root=TakeInputLevelWise();
     PrintTreeLevelWise(root);
     cout<<endl;
+    pair<int, int> p=heightDiameter(root);
+    cout<<"height: "<<p.first<<endl;
+    cout<<"diameter: "<<p.second<<endl;
+    /*
     cout<<numNodes(root)<<endl;
     cout<<isNodePresent(root, 0)<<endl;
     cout<<"height: "<<height(root)<<endl;
     mirrorBinaryTree(root);
     cout<<endl;
+    */
     delete root;
 }
