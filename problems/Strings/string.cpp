637a34e4d4cc2d4b19b665ae67ad8ee97319044e
Abhishek singh <abhisheksingh6716@gmail.com>
2020-10-18 17:35:44 +0530

    added more files

---
+++ problems/Strings/string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int x='0';
+    int y='1';
+    int z=y-x;
+    cout<<x<<endl;
+    cout<<y<<endl;
+    cout<<z<<endl;
+}
