d2a9603830bf9bf952d0661b769ffd771b92de1a
Abhishek singh <abhisheksingh6716@gmail.com>
2020-11-22 01:07:11 +0530

    modified file

--- Ds-Algo/DP-2/knapsack.cpp
+++ Ds-Algo/DP-2/knapsack.cpp
@@ -4,38 +4,76 @@ using namespace std;
 
 //by DP method
 int knapsack_dp(int *wts, int *values, int n, int maxwts){
-    int p=n+1;
-    int q=maxwts;
-    int **output=new int*[p+1];
-    for(int i=0; i<p+1; i++){
-        output[i]=new int[q+1];
+    int a=n;
+    int b=maxwts;
+    int **output=new int*[a+1];
+    for(int i=0; i<a+1; i++)
+    {
+        output[i]=new int[b+1];
     }
-    //filling 1st row
-    for(int j=0; j<q+1; j++){
+    for(int j=0; j<b+1; j++)
+    {
         output[0][j]=0;
     }
-    //filling 1st column
-    for(int i=0; i<p+1; i++){
+    for(int i=0; i<a+1; i++)
+    {
         output[i][0]=0;
     }
-    //filling other cells
-    for(int i=1; i<p+1; i++){
-        for(int j=1; j<q+1; j++){
-            if(wts[i]>=maxwts){
-                output[i][j]=output[i-1][maxwts];
+    for(int i=1; i<a+1; i++)
+    {
+        for(int j=1; j<b+1; j++)
+        {
+            if(wts[i-1]>=j)
+            {
+                output[i][j]=output[i-1][j];
             }
-            int x=output[i-1][maxwts-wts[i]]+values[i];
-            int y=output[i-1][maxwts];
+            int x=output[i-1][j-wts[i-1]]+values[i-1];
+            int y=output[i-1][j];
             output[i][j]=max(x, y);
         }
     }
-    return output[p][q];
+    return output[a][b];
 }
 
-
-
-
-//Can't belive memoisation method is done by me itself :-)
+/// on second time achived better solution 
+/// memoisation
+int helper(int *wts, int *val, int n, int maxwts, int **output)
+{
+    int a=n;
+    int b=maxwts;
+    if(n==0 || maxwts<=0){
+        return 0;
+    }
+    if(output[a][b]!=-1){
+        return output[a][b];
+    }
+    int ans;
+    if(wts[0]>maxwts)
+    {
+        ans=helper(wts+1, val+1, n-1, maxwts, output);
+    }
+    else
+    {
+        int x=helper(wts+1, val+1, n-1, maxwts-wts[0], output)+val[0];
+        int y=helper(wts+1, val+1, n-1, maxwts, output);
+        ans=max(x, y);
+    }
+    output[a][b]=ans;
+    return ans;
+}
+int knapsack_mem(int *wts, int *val, int n, int maxwts)
+{
+    int **output=new int*[n+1];
+    for(int i=0; i<n+1; i++){
+        output[i]=new int[maxwts+1];
+        for(int j=0; j<maxwts+1; j++){
+            output[i][j]=-1;
+        }
+    }
+    return helper(wts, val, n, maxwts, output);
+}
+/*
+// first time code written by me having lots of mistakes
 int knapsack_mem(int *wts, int *values, int n, int maxwts, int **output){
     int p=n+1;
     int q=maxwts+1;
@@ -70,6 +108,7 @@ int knapsack_mem(int *wts, int *values, int n, int maxwts){
     }
     return knapsack_mem(wts, values, n, maxwts, output);
 }
+*/
 
 
 
